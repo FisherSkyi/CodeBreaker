72391fd51edcdcfb2892c3cf9caf691e3b54b0e7
YuLetian <yu.letian66@gmail.com>
2025-12-31 22:56:28 +0800

    Initial Commit

---
+++ Assignment0/Assignment0App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment0App.swift
+//  Assignment0
+//
+//  Created by SKY on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Assignment0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
