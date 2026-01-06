c8d12ccbcef2560efe380ef1a0a14cf405a2a50a
YuLetian <yu.letian66@gmail.com>
2026-01-06 17:18:01 +0800

    Lec 4

--- CodeBreaker/CodeBreaker.swift
+++ CodeBreaker/CodeBreaker.swift
@@ -5,25 +5,70 @@
 //  Created by SKY on 6/1/26.
 //
 
-import Foundation
+import SwiftUI
 
+typealias Peg = Color
+
+// Model
 struct CodeBreaker {
-    var masterCode: Code
-    var guess: Code
-    var attempts: [Code]
-    var pegChoice: [Peg]
+    var masterCode = Code(kind: .master)
+    var guess = Code(kind: .guess)
+    var attempts: [Code] = []
+    let pegChoice: [Peg]
     
-    mutating
+    init(pegChoice: [Peg] = [.red, .green, .blue, .yellow]) {
+        self.pegChoice = pegChoice // initializer is allowed to assign initial value of a `let`
+        masterCode.randomize(from: pegChoice)
+        print(masterCode)
+    }
+    
+    mutating func changeGuessPeg(at index: Int) {
+        let existingPeg = guess.pegs[index]
+        if let indexOfExistingPegInPegChoices = pegChoice.firstIndex(of: existingPeg) {
+            let newPegIndex = (indexOfExistingPegInPegChoices + 1) % pegChoice.count
+            let newPeg = pegChoice[newPegIndex]
+            guess.pegs[index] = newPeg
+        } else {
+            guess.pegs[index] = pegChoice.first ?? Code.missing
+        }
+    }
+    
+    mutating func attemptGuess() {
+        var attempt = guess // value type
+        attempt.kind = .attempt(guess.match(against: masterCode))
+        attempts.append(attempt)
+    }
 }
 
 struct Code {
-    var kind: Kind
-    var pegs: [Peg]
+    var kind: Code.Kind
+    var pegs: [Peg] = Array(repeating: Code.missing, count: 4)
     
-    enum Kind {
+    static let missing = Peg.clear
+    
+    enum Kind: Equatable {
         case master
         case guess
-        case attempt
+        case attempt([Match])
         case unknown
     }
+    
+    mutating func randomize(from pegChoices: [Peg]) {
+        for index in pegChoices.indices {
+            pegs[index] = pegChoices.randomElement() ?? Code.missing
+        }
+    }
+    
+    var matches: [Match] {
+        switch kind {
+        // If kind is .attempt and it has an associated value, bind that value to a local constant named matches.
+        case .attempt(let matches): return matches
+        default: return []
+        }
+    }
+    
+    func match(against otherCode: Code) -> [Match] {
+        return [.exact, .exact, .exact, .exact]
+    }
 }
+
--- CodeBreaker/CodeBreakerView.swift
+++ CodeBreaker/CodeBreakerView.swift
@@ -8,25 +8,58 @@
 import SwiftUI
 
 struct CodeBreakerView: View {
-    let game: CodeBreaker = CodeBreaker()
+    // `@State` create a State Class
+    @State var game = CodeBreaker()
     
     var body: some View {
         VStack{
-            pegs(colors: [.red, .green, .yellow, .blue])
-            pegs(colors: [.blue, .red, .yellow, .blue])
-            pegs(colors: [.red, .green, .blue, .red])
+            view(for: game.masterCode)
+            view(for: game.guess)
+            Divider()
+            ScrollView {
+                ForEach(game.attempts.indices.reversed(), id: \.self) { index in
+                    view(for: game.attempts[index])
+                }
+               
+            }
         }
         .padding()
     }
     
-    func pegs(colors: Array<Color>) -> some View {
-        HStack {
-            ForEach(colors.indices, id: \.self) { index in
-                RoundedRectangle(cornerRadius: 10)
-                    .aspectRatio(1, contentMode: .fit)
-                    .foregroundStyle(colors[index])
+    var guessButton: some View {
+        Button("Guess") {
+            withAnimation {
+                game.attemptGuess()
             }
-            MatchMarker(matches: [.exact, .inexact, .nomatch, .exact])
+        }.font(.system(size: 80)).minimumScaleFactor(0.1)
+    }
+    
+    // Note there should never be mutating function in View
+    func view(for code: Code) -> some View {
+        HStack {
+            ForEach(code.pegs.indices, id: \.self) { index in
+                RoundedRectangle(cornerRadius: 10)
+                    .overlay {
+                        if code.pegs[index] == Code.missing {
+                            RoundedRectangle(cornerRadius: 10)
+                                .strokeBorder(Color.gray)
+                        }
+                    }
+                    .contentShape(Rectangle())
+                    .aspectRatio(1, contentMode: .fit)
+                    .foregroundStyle(code.pegs[index])
+                    .onTapGesture {
+                        if code.kind == Code.Kind.guess {
+                            game.changeGuessPeg(at: index)
+                        }
+                    }
+            }
+            MatchMarker(matches: code.matches)
+                .overlay {
+                    if code.kind == .guess {
+                        guessButton
+                    }
+                }
         }
     }
 }
